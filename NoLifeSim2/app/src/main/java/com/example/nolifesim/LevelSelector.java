827c5844a86a7e29640b1d4ca9990c4eeae021db
hanterpunda <mxu2@vcu.edu>
2021-10-15 21:28:30 -0400

    Updating Base Game and Implementing UI

---
+++ NoLifeSim2/app/src/main/java/com/example/nolifesim/LevelSelector.java
@@ -0,0 +1,14 @@
+package com.example.nolifesim;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LevelSelector extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_level_selector);
+    }
+}
--- NoLifeSim2/app/src/main/java/com/example/nolifesim/MainActivity.java
+++ NoLifeSim2/app/src/main/java/com/example/nolifesim/MainActivity.java
@@ -6,24 +6,44 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button button;
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                openMainActivity2();
-            }
-        });
+        Button buttonLevel = findViewById(R.id.buttonLevel);
+        Button buttonInventory = findViewById(R.id.buttonInventory);
+        Button buttonRoll = findViewById(R.id.buttonRoll);
+
+        buttonLevel.setOnClickListener(MainActivity.this);
+        buttonInventory.setOnClickListener(MainActivity.this);
+        buttonRoll.setOnClickListener(MainActivity.this);
     }
-    public void openMainActivity2(){
-        Intent intent = new Intent(this, MainActivity2.class);
-        startActivity(intent);
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.buttonLevel:
+                Toast.makeText(MainActivity.this, "Button Level selected", Toast.LENGTH_SHORT).show();
+                Intent levels = new Intent(this, LevelSelector.class);
+                startActivity(levels);
+                break;
+            case R.id.buttonInventory:
+                Toast.makeText(MainActivity.this, "Button Inventory", Toast.LENGTH_SHORT).show();
+                Intent Inventory = new Intent(this, Inventory.class);
+                startActivity(Inventory);
+                break;
+            case R.id.buttonRoll:
+                Toast.makeText(MainActivity.this, "Button Rolls", Toast.LENGTH_SHORT).show();
+                Intent Roll = new Intent(this, Roll.class);
+                startActivity(Roll);
+                break;
+        }
     }
+
 }
